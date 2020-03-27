767bac32d2225a2825bca42c64901c04aa0ced6e
Rafal <rmf@europe.com>
2020-03-27 06:51:27 +0100

    the title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title1: string = 'Angular-In-Examples';
-  title2: string = 'navigation, {path: "**", component: ......}';
+  title2: string = 'ActivatedRoute, przekazanie danych statycznych do trasy';
 }
