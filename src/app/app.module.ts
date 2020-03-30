260b1295da13f5ba0655f1bc2d75d3fe32c619f2
Rafal <rmf@europe.com>
2020-03-30 07:12:55 +0200

    new component four

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { TwoComponent } from './two/two.component';
 import { ThreeComponent } from './three/three.component';
 import { Routes, RouterModule } from '@angular/router';
 import { WarningComponent } from './warning/warning.component';
+import { FourComponent } from './four/four.component';
 
 export const appRouters: Routes = [
   {path: '', component: OneComponent},
   {path: 'one', component: OneComponent},
   {path: 'two', component: TwoComponent},
   {path: 'three', component: ThreeComponent, data: [{version: '1.3.8'}]},
+  {path: 'four', component: FourComponent},
   {path: '**', component: WarningComponent}
 ];
 @NgModule({
@@ -22,6 +24,7 @@ export const appRouters: Routes = [
     TwoComponent,
     ThreeComponent,
     WarningComponent,
+    FourComponent,
   ],
   imports: [
     BrowserModule,
